924cc5ecf6b5007ab1028c3c2ac8b2738d517698
Selene <spachecor03@gmail.com>
2024-12-05 22:58:09 +0100

    tarde 05122024

--- php/tema3/PawTronics/index.php
+++ php/tema3/PawTronics/index.php
@@ -1,21 +1,65 @@
+<?php
+$activeTab = isset($_GET['tab']) ? $_GET['tab'] : 'Comerciales';
+$insertButton = isset($_GET['insert']) ? $_GET['insert'] : '';
+if ($insertButton !== '')
+    $activeTab = '';
+?>
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
+    <title>Gestionador - Pawtronics</title>
+    <!--Estilos personalizados-->
+    <link rel="stylesheet" href="assets/css/normalize.css">
+    <link rel="stylesheet" href="assets/css/index.css">
+    <link rel="stylesheet" href="assets/css/global.css">
 </head>
+
 <body>
-
-<?php
-
-//LISTA DE PENDIENTES
-//todo agregar el .inc.php en todos los archivos agregables
-//todo agregar login
-//todo agregar a db tabla para el login
-//todo agregar interfaz para visualizar la info, siendo una web para que un supervisor vea las ventas de los comerciales(ver img)
-//todo agregar encriptacion passsword con password_hash
-?>
-
+    <header>
+        <nav>
+            <div class="logo-mas-marca">
+                <div class="logo"></div>
+                <p class="marca">Pawtronics</p>
+            </div>
+            <div class="menu-mas-cerrar-sesion">
+                <a href="?tab=Comerciales" class="pestana <?= $activeTab === 'Comerciales' ? 'active' : '' ?>">
+                    Comerciales
+                </a>
+                <a href="?tab=Productos" class="pestana <?= $activeTab === 'Productos' ? 'active' : '' ?>">
+                    Productos
+                </a>
+                <a href="?tab=Ventas" class="pestana <?= $activeTab === 'Ventas' ? 'active' : '' ?>">
+                    Ventas
+                </a>
+            </div>
+        </nav>
+    </header>
+    <main>
+        <!--boton flotante agregar nuevos registros-->
+        <div class="boton-flotante">
+            <input type="checkbox" id="btn-insert">
+            <div class="opciones">
+                <a href="?insert=Comerciales" title="Comerciales">Comerciales</a>
+                <a href="?insert=Productos" title="Productos">Productos</a>
+                <a href="?insert=Ventas" title="Ventas">Ventas</a>
+            </div>
+            <label for="btn-insert" class="insert-box"></label>
+        </div>
+        <!--Contenido-->
+        <div class="contenido-main">
+            <?php
+            if($activeTab!=='')include "views/gestionar.php";
+            else include "views/insertar.php";
+            ?>
+        </div>
+    </main>
+    <footer>
+        <hr>
+        <p>&copy; 2024 Pawtronics</p>
+    </footer>
 </body>
+
 </html>
--- php/tema3/PawTronics/views/insertar.php
+++ php/tema3/PawTronics/views/insertar.php
@@ -1,17 +1,20 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="assets/css/gestionar.css">
+    <link rel="stylesheet" href="assets/css/insertar.css">
 </head>
+
 <body>
-    <div class="contenido-gestionar">
-    <h2>Aqui toy!!! soy las tablas</h2>
-    <?php
-    echo $activeTab;
-    ?>
+    <div class="contenido-insertar">
+        <h2>Aqui toy!!! soy la insercion</h2>
+        <?php
+        echo $insertButton;
+        ?>
     </div>
 </body>
+
 </html>
